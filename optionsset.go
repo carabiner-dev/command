88d4c1b486a5439ff277d8b50f1c4fa2f9be09f1
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-09-12 12:14:31 -0600

    Small readme
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

---
+++ optionsset.go
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package command
+
+import "github.com/spf13/cobra"
+
+// OptionsSet is an interface that defines the functions options set need
+// to implement to make them reusable.
+type OptionsSet interface {
+	AddFlags(*cobra.Command)
+	Verify() error
+}
